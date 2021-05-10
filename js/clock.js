86a0fa3d4b57be0b55c0bf73506ea66d5e152c68
Thijs Boom <twboom@hotmail.nl>
2021-05-10 21:59:26 +0200

    Fixed this

---
+++ js/clock.js
@@ -0,0 +1,63 @@
+const clock = [];
+
+clock.time = 300000 // Countdown time in milliseconds
+
+clock.place = clock.time
+
+// Start countdown
+clock.start = _ => {
+    clock.interval = setInterval(_ => {
+
+        const distance = clock.place
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        const milliseconds = distance % (1000 * 60) - seconds * 1000;
+
+        const time = `${clock.fixString(minutes)}:${clock.fixString(seconds)}:${clock.fixString(milliseconds, true)}`
+        document.getElementById('clock').innerText = time;
+
+        clock.place -= 10
+
+        if (clock.place === 0) {
+            clearInterval(clock.interval);
+            document.getElementById('clock').innerText = '00:00:000';
+        };
+
+    }, 10)
+}
+
+clock.fixString = (int, ms) => {
+
+    let str = String(int)
+
+    if (ms) {
+        str = str.slice(0, -1);
+        const random = Math.floor(Math.random() * 10)
+        str += random
+    }
+
+    if (str.length <= 1) {
+        str = '0' + str;
+    }
+
+    if (str.length <= 2 && ms) {
+        str = '0' + str;
+    }
+
+    return str
+}
+
+document.addEventListener('visibilitychange', _ => {
+    if (document.visibilityState === 'visible') {
+        clock.place = clock.time
+        clock.start();
+    };
+});
+
+window.addEventListener('load', _ => {
+    console.log(document.visibilityState)
+    if (document.visibilityState === 'visible') {
+        clock.place = clock.time
+        clock.start();
+    };
+})
